6fec0165d7cb9c12777c85ddeda8d1a65fd1011c
J Cleydson Silva <126107571+jcleydsonsilva@users.noreply.github.com>
2023-05-24 08:17:11 -0400

    Update ORFome.py

--- ORFome.py
+++ ORFome.py
@@ -172,7 +172,7 @@ def main():
 	#fasta = get_data('Sequence.fa')
 	fasta = get_data(sys.argv[1])
 	file_orfs = 'ORFs-cds.fasta'
-	file_prot = 'ORFs-to-aminoadics_sequence.fasta'
+	file_prot = 'ORFs-to-amino-acids-sequence.fasta'
 	file_orfs_write = open(file_orfs, 'w')
 	file_prot_write = open(file_prot, 'w')
 	for key in fasta:
